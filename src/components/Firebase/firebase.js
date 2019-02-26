7b70573a020c3e3330e06a34012ef66ed1bbc4bb
Bob Zabor <bobzabor@detroitlabs.com>
2019-02-26 16:49:28 -0500

    saving

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,4 +1,5 @@
 import app from "firebase/app";
+import "firebase/auth";
 
 const config = {
   apiKey: "AIzaSyDmLiFSI_4mH80n0eo7kU1MH7sgfJE8cco",
@@ -12,7 +13,25 @@ const config = {
 class Firebase {
   constructor() {
     app.initializeApp(config);
+    this.auth = app.auth();
   }
+
+  // *** Auth API ***
+  doLogOut = () => this.auth.signOut();
+
+  doLogIn = () => {
+    console.log("ENTER firebase.js doLogIn()");
+    const provider = new this.auth.GoogleAuthProvider();
+    this.auth.signInWithPopup(provider);
+    // .then(auth => {
+    //   console.log("SIGN IN SUCCESS!");
+    //   return true;
+    // })
+    // .catch(error => {
+    //   console.error(error);
+    //   return false;
+    // });
+  };
 }
 
 export default Firebase;
